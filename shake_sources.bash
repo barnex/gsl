fbf966bc1ada4b3da73b751d0d76b29040b55e86
Arne Vansteenkiste <barnex@gmail.com>
2014-10-23 13:53:02 +0200

    cblas compiles

--- shake_sources.bash
+++ shake_sources.bash
@@ -14,16 +14,17 @@ for f in $d/*.c $d/*.h; do
 done
 
 # now for src subdirs:
-dirs="gsl blas block bspline" 
+dirs="gsl blas block bspline cblas" 
 
 for d in $dirs; do
 	d=$src/$d
 	for f in $d/*.c $d/*.h; do
-			sed -r 's/#include "([a-z|_]*)\.h"/#include "'$(basename $d)'_\1\.h"/g' $f | \
+			bd=$(basename $d)
+			sed -r 's/#include "([a-z|_|0-9]*)\.h"/#include "'$bd'_\1\.h"/g' $f | \
 			sed -r 's/#include "[a-z]*_templates_([o|f|n]*)\.h"/#include "templates_\1\.h"/g'  | \
-			sed -r 's/#include "([a-z|_|\.]*)_source.c"/#include "'$(basename $d)'_\1_source.c.inc"/g' | \
+			sed -r 's/#include "([a-z|_|\.]*)\.c"/#include "'$bd'_\1.c.inc"/g' | \
 			sed -r 's/#include <gsl\/([a-z|_|\.]*)>/#include "gsl_\1"/g'  | \
-			sed -r 's/#include <(config|gsl_version|gsl_types)\.h>/#include "\1\.h"/g' > $dst/$(basename $d)_$(basename $f)
+			sed -r 's/#include <(config|gsl_version|gsl_types)\.h>/#include "\1\.h"/g' > $dst/$bd'_'$(basename $f)
 	done
 done
 
@@ -35,6 +36,9 @@ rm $dst/*_\*.* # stray files
 for f in $dst/*_source.c; do
 		mv $f $f.inc
 done
+
+#quirks
+mv $dst/cblas_hypot.c $dst/cblas_hypot.c.inc
 
 rm $dst/*_test_*.c $dst/*_test.c
 
