e749d6a922c0916b4831bc223a3f0a4b1ba54b11
Arne Vansteenkiste <barnex@gmail.com>
2014-10-23 11:58:32 +0200

    block compiles

--- shake_sources.bash
+++ shake_sources.bash
@@ -4,28 +4,37 @@
 src="gsl-1.16" #original source
 dst="cleaned"
 
-rm -rf $dst
-mkdir $dst
+rm -f $dst/*.c $dst/*.h $dst/*.o
 
-
-# in main src dir
-# replace '#include <xxx.h>' by '#include "xxx.h"'
-# and '#include <gsl/xxx.h>' by '#include "gsl_xxx.h"'
+#in main src dir
 d=$src
 for f in $d/*.c $d/*.h; do
-		sed -r 's/#include <([a-z|\/|_]*\.h)>/#include "\1"/g' $f | sed 's/#include "gsl\//#include "gsl_/g' > $dst/$(basename $f)
+		sed -r 's/#include <(config|gsl_version|gsl_types)\.h>/#include "\1\.h"/g' $f | \
+		sed -r 's/#include <gsl\/([a-z|_]*)\.h>/#include "gsl_\1\.h"/g' > $dst/$(basename $f)
 done
 
 # now for src subdirs:
-dirs="gsl blas"
+dirs="gsl blas block" 
 
 for d in $dirs; do
 	d=$src/$d
 	for f in $d/*.c $d/*.h; do
-			sed -r 's/#include <([a-z|_]*)\/([a-z|_]*)\.h>/#include "\1_\2\.h"/g' $f | sed -r 's/#include <([a-z|\/|_]*\.h)>/#include "\1"/g' > $dst/$(basename $d)_$(basename $f)
+			sed -r 's/#include "([a-z|_|\.]*)_source.c"/#include "'$(basename $d)'_\1_source.c.inc"/g' $f | \
+			sed -r 's/#include <gsl\/([a-z|_|\.]*)>/#include "gsl_\1"/g'  | \
+			sed -r 's/#include <(config|gsl_version|gsl_types)\.h>/#include "\1\.h"/g' > $dst/$(basename $d)_$(basename $f)
 	done
 done
 
 
+rm $dst/*_\*.* # stray files
 
-#(cd $dst && gcc -c *.c)
+# some people think it's a good idea to include .c files.
+# move them aside so they don't get compiled twice
+for f in $dst/*_source.c; do
+		mv $f $f.inc
+done
+
+rm $dst/*_test_*.c $dst/*_test.c
+
+
+(cd $dst && gcc -c -w *.c)
