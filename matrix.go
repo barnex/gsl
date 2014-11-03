674295589f0675be38da90c403485859412b98da
Arne Vansteenkiste <barnex@gmail.com>
2014-11-03 09:30:31 +0100

    d/c/z submatrix

--- matrix.go
+++ matrix.go
@@ -101,10 +101,45 @@ const (
 // Returns a submatrix of the matrix M.
 // The upper-left element of the submatrix is the element (k1,k2) of the original matrix.
 // The submatrix has n1 rows and n2 columns. The physical number of columns (stride) in memory is unchanged.
-// Mathematically, the (i,j)-th element of the new matrix is given by:
-// 	m'(i,j) = data[(k1*m.stride + k2) + i*m.stride + j]
+// The returned matrix shares underlying storage with the original: modifying either one will affect the other.
 func SSubmatrix(M [][]float32, k1, k2, n1, n2 int) [][]float32 {
 	m := make([][]float32, n1)
+	for i := range m {
+		m[i] = M[k1+i][k2 : k2+n2]
+	}
+	return m
+}
+
+// Returns a submatrix of the matrix M.
+// The upper-left element of the submatrix is the element (k1,k2) of the original matrix.
+// The submatrix has n1 rows and n2 columns. The physical number of columns (stride) in memory is unchanged.
+// The returned matrix shares underlying storage with the original: modifying either one will affect the other.
+func DSubmatrix(M [][]float64, k1, k2, n1, n2 int) [][]float64 {
+	m := make([][]float64, n1)
+	for i := range m {
+		m[i] = M[k1+i][k2 : k2+n2]
+	}
+	return m
+}
+
+// Returns a submatrix of the matrix M.
+// The upper-left element of the submatrix is the element (k1,k2) of the original matrix.
+// The submatrix has n1 rows and n2 columns. The physical number of columns (stride) in memory is unchanged.
+// The returned matrix shares underlying storage with the original: modifying either one will affect the other.
+func CSubmatrix(M [][]complex64, k1, k2, n1, n2 int) [][]complex64 {
+	m := make([][]complex64, n1)
+	for i := range m {
+		m[i] = M[k1+i][k2 : k2+n2]
+	}
+	return m
+}
+
+// Returns a submatrix of the matrix M.
+// The upper-left element of the submatrix is the element (k1,k2) of the original matrix.
+// The submatrix has n1 rows and n2 columns. The physical number of columns (stride) in memory is unchanged.
+// The returned matrix shares underlying storage with the original: modifying either one will affect the other.
+func ZSubmatrix(M [][]complex128, k1, k2, n1, n2 int) [][]complex128 {
+	m := make([][]complex128, n1)
 	for i := range m {
 		m[i] = M[k1+i][k2 : k2+n2]
 	}
--- matrixexamples_test.go
+++ matrixexamples_test.go
@@ -35,3 +35,79 @@ func ExampleMakeComplex128Matrix() {
 	//Output:
 	//[[(0+0i) (0+0i) (0+0i)] [(0+0i) (0+0i) (0+0i)]]
 }
+
+func ExampleSSubmatrix() {
+	M := MakeFloat32Matrix(3, 4)
+	for i := range M {
+		for j := range M[i] {
+			M[i][j] = float32(10*i + j)
+		}
+	}
+	fmt.Println(M)
+
+	k1, k2 := 0, 1 // offset
+	n1, n2 := 2, 3 // size
+	m := SSubmatrix(M, k1, k2, n1, n2)
+	fmt.Println(m)
+
+	//Output:
+	//[[0 1 2 3] [10 11 12 13] [20 21 22 23]]
+	//[[1 2 3] [11 12 13]]
+}
+
+func ExampleDSubmatrix() {
+	M := MakeFloat64Matrix(3, 4)
+	for i := range M {
+		for j := range M[i] {
+			M[i][j] = float64(10*i + j)
+		}
+	}
+	fmt.Println(M)
+
+	k1, k2 := 0, 1 // offset
+	n1, n2 := 2, 3 // size
+	m := DSubmatrix(M, k1, k2, n1, n2)
+	fmt.Println(m)
+
+	//Output:
+	//[[0 1 2 3] [10 11 12 13] [20 21 22 23]]
+	//[[1 2 3] [11 12 13]]
+}
+
+func ExampleCSubmatrix() {
+	M := MakeComplex64Matrix(3, 4)
+	for i := range M {
+		for j := range M[i] {
+			M[i][j] = complex(float32(i), float32(j))
+		}
+	}
+	fmt.Println(M)
+
+	k1, k2 := 0, 1 // offset
+	n1, n2 := 2, 3 // size
+	m := CSubmatrix(M, k1, k2, n1, n2)
+	fmt.Println(m)
+
+	//Output:
+	//[[(0+0i) (0+1i) (0+2i) (0+3i)] [(1+0i) (1+1i) (1+2i) (1+3i)] [(2+0i) (2+1i) (2+2i) (2+3i)]]
+	//[[(0+1i) (0+2i) (0+3i)] [(1+1i) (1+2i) (1+3i)]]
+}
+
+func ExampleZSubmatrix() {
+	M := MakeComplex128Matrix(3, 4)
+	for i := range M {
+		for j := range M[i] {
+			M[i][j] = complex(float64(i), float64(j))
+		}
+	}
+	fmt.Println(M)
+
+	k1, k2 := 0, 1 // offset
+	n1, n2 := 2, 3 // size
+	m := ZSubmatrix(M, k1, k2, n1, n2)
+	fmt.Println(m)
+
+	//Output:
+	//[[(0+0i) (0+1i) (0+2i) (0+3i)] [(1+0i) (1+1i) (1+2i) (1+3i)] [(2+0i) (2+1i) (2+2i) (2+3i)]]
+	//[[(0+1i) (0+2i) (0+3i)] [(1+1i) (1+2i) (1+3i)]]
+}
